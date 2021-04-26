d6d87cc2b8abb3d1c9361ebdf4991097c4cb282c
Heri Heryanto <Heri.Heryanto-EXT@xapiens.id>
2021-04-26 12:48:47 +0700

    chore dashboard

---
+++ src/utils/responsive.js
@@ -0,0 +1,37 @@
+export const ResponsivePromo = {
+  superLargeDesktop: {
+    breakpoint: { max: 4200, min: 3200 },
+    items: 2
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 2
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+}
+
+export const ResponsiveClass = {
+  superLargeDesktop: {
+    breakpoint: { max: 4200, min: 3200 },
+    items: 3
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+}
